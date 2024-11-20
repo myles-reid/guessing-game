8b645e9970e73a49bfc3a5afb812a1477a4773e5
Myles Reid <myles@reidsmailbox.com>
2024-11-20 10:00:36 -0600

    remaining guesses added

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,18 +12,6 @@ function listen(event, element, callback) {
   return element.addEventListener(event, callback);
 }
 
-function create(element, scope = document) {
-  return scope.createElement(element);
-}
-
-function addChild(element, child) {
-  return element.appendChild(child);
-}
-
-function addFirstChild(element, child) {
-  return element.insertBefore(child, element.firstChild);
-}
-
 function addClass(element, text) {
   return element.classList.add(text);
 }
@@ -34,5 +22,31 @@ function removeClass(element, text) {
 
 function toggleClass(element, text) {
   return element.classList.toggle(text);
+}
+
+const playAgain = select('.again');
+const restart = select('.restart');
+const start = select('.start');
+const randomNum = select('.front-face');
+const remainingGuesses = select('.guess-count');
+const input = select('.guess');
+const output = select('.output');
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+remainingGuesses.innerText = 5;
+
+function setRemainingGuesses() {
+  let currentGuess = parseInt(remainingGuesses.innerText);
+  if (currentGuess > 0) {
+    currentGuess -= 1;
+    remainingGuesses.innerText = currentGuess
+  }
+}
+
+function setRandomNumber() {
+  return randomNum.innerText = setRandomNumber(1, 50);
 }
 
